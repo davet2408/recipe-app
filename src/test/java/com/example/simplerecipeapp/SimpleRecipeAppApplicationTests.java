618d5ecdd476c4138c84c1b7fe4d470e91c3e137
David Temple <david.temple@kcl.ac.uk>
2020-10-08 08:51:22 +0100

    Add initial Spring Boot app and index page.

---
+++ src/test/java/com/example/simplerecipeapp/SimpleRecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simplerecipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleRecipeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
