576d576b451545703191488fa26429f1fd1570ba
David Temple <david.temple@kcl.ac.uk>
2020-10-21 13:31:17 +0100

    Refactor to use project lombok

--- src/main/java/com/example/simplerecipeapp/model/Ingredient.java
+++ src/main/java/com/example/simplerecipeapp/model/Ingredient.java
@@ -1,8 +1,13 @@
 package com.example.simplerecipeapp.model;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
 import javax.persistence.*;
 import java.math.BigDecimal;
 
+@Data
+@EqualsAndHashCode(exclude = {"recipe"})
 @Entity
 public class Ingredient {
 
@@ -18,16 +23,7 @@ public class Ingredient {
     @OneToOne
     private UnitOfMeasure unitOfMeasure;
 
-    public UnitOfMeasure getUnitOfMeasure() {
-        return unitOfMeasure;
-    }
-
-    public void setUnitOfMeasure(UnitOfMeasure unitOfMeasure) {
-        this.unitOfMeasure = unitOfMeasure;
-    }
-
     public Ingredient() {
-
     }
 
     public Ingredient(String description, BigDecimal amount, UnitOfMeasure unitOfMeasure) {
@@ -43,35 +39,4 @@ public class Ingredient {
         this.unitOfMeasure = unitOfMeasure;
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public BigDecimal getAmount() {
-        return amount;
-    }
-
-    public void setAmount(BigDecimal amount) {
-        this.amount = amount;
-    }
-
-    public Recipe getRecipe() {
-        return recipe;
-    }
-
-    public void setRecipe(Recipe recipe) {
-        this.recipe = recipe;
-    }
 }
